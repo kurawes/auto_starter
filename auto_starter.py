3509c80057ce15b3ad519307ecdf711415d30aaa
kurawes <erkiraiv@gmail.com>
2021-11-13 00:03:17 +0200

    git

---
+++ auto_starter.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import os
+from tkinter import filedialog, Text
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=700, width=700, bg="light blue")
+canvas.pack()
+
+root.mainloop()
