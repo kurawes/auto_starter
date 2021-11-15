0653e060339c1553ae68232d7c1d7c3481df1410
kurawes <erkiraiv@gmail.com>
2021-11-15 22:14:15 +0200

    add comments

--- auto_starter.py
+++ auto_starter.py
@@ -2,9 +2,15 @@ import tkinter as tk
 import os
 from tkinter import filedialog, Text
 
+
+# main anchor element
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=700, width=700, bg="light blue")
-canvas.pack()
 
+# creates the program window
+canvas = tk.Canvas(root, height=700, width=700, bg="light blue")
+canvas.pack()   # attaches canvas to the root
+
+
+# Runs the main loop
 root.mainloop()
