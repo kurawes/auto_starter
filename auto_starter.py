90042c88b54a3d86feb083521bac181be6b7fd76
kurawes <erkiraiv@gmail.com>
2021-11-23 23:31:31 +0200

    add global color variables for easy of use

--- auto_starter.py
+++ auto_starter.py
@@ -3,8 +3,18 @@ import os
 from tkinter import Grid, filedialog, Text
 
 
+# Color variables for easyer access
+TEXT_FG = "#c8ccd1"
+TEXT_BG = "#2b2c33"
+BUTTON_FG = "#ebf0f6"
+BUTTON_BG = "#19191b"
+FRAME_BG = "#2b2c33"
+CANVAS_BG = "#19191b"
+
+
 # main anchor element
 root = tk.Tk()
+root.title("Auto Starter")
 
 apps = []   # a list for added apps/executables
 
@@ -31,13 +41,18 @@ def add_app():
 
     # show the added apps in the frame
     for app in apps:
-        label = tk.Label(frame, text=app, bg="light blue")
-        label.pack()   # attach the program name to the frame and align them to the left
+        label = tk.Label(frame, text=app, fg=TEXT_FG, bg=TEXT_BG)
+        label.pack()   # attach the program name to the frame
 
 
 def run_apps():
     for app in apps:
         os.startfile(app)   # loops over the list and runs the apps
+
+
+def remove_selected_app():
+    item_id = 3
+    print(item_id)
 
 
 def clear_all_executables():
@@ -47,30 +62,30 @@ def clear_all_executables():
     
 
 # create the program window
-canvas = tk.Canvas(root, height=700, width=700, bg="light blue")
+canvas = tk.Canvas(root, height=700, width=500, bg=CANVAS_BG)
 canvas.pack(fill="both", expand = True)   # attach canvas to the root, fill the canvas even when rezising
 
 
 # create frame inside the main window
-frame = tk.Frame(root, bg="gray")
+frame = tk.Frame(root, bg=FRAME_BG)
 frame.place(relheight=0.9, relwidth=0.9, relx=0.05, rely=0.05)
 
 # create secondary frame for better button control
-frame2 = tk.Frame(root, bg="gray")
+frame2 = tk.Frame(root, bg=FRAME_BG)
 frame2.place(relheight=0.05, relwidth=0.9, relx=0.05, rely=0.9)
 
 
 # add buttons to the frame2
-open_file = tk.Button(frame2, text="Open App", fg="black", bg="light blue", command=add_app)
+open_file = tk.Button(frame2, text="Open App", fg=BUTTON_FG, bg=BUTTON_BG, command=add_app)
 open_file.grid(row=0, column=0, sticky="nsew") # attach open file button to the frame
 
-run_all_apps = tk.Button(frame2, text="Run Apps",fg="black", bg="light blue", command=run_apps)
+run_all_apps = tk.Button(frame2, text="Run Apps",fg=BUTTON_FG, bg=BUTTON_BG, command=run_apps)
 run_all_apps.grid(row=0, column=1, sticky="nsew")
 
-remove_app = tk.Button(frame2, text="Remove App", fg="black", bg="light blue")
+remove_app = tk.Button(frame2, text="Remove App", fg=BUTTON_FG, bg=BUTTON_BG, command=remove_selected_app)
 remove_app.grid(row=0, column=2, sticky="nsew")
 
-clear_all_apps = tk.Button(frame2, text="Clear all", fg="black", bg="light blue", command=clear_all_executables)
+clear_all_apps = tk.Button(frame2, text="Clear all", fg=BUTTON_FG, bg=BUTTON_BG, command=clear_all_executables)
 clear_all_apps.grid(row=0, column=3, sticky="nsew")
 
 # Configure the columns and rows of the buttons
@@ -80,7 +95,7 @@ frame2.grid_rowconfigure(0, weight=1)
 
 # add the programs from the save file to the frame when starting
 for app in apps:
-    label = tk.Label(frame, text=app)
+    label = tk.Label(frame, text=app, fg=TEXT_FG, bg=TEXT_BG)
     label.pack()
 
 
